e249e25e15a8df4f9a661fe191074e4582234c66
unknown <bfengos@gmail.com>
2022-01-13 01:49:11 -0500

    Adam user model params for metamask login

--- models/user.js
+++ models/user.js
@@ -12,6 +12,17 @@ const UserSchema = new Schema({
       admin: Boolean,
     },
   ],
+  nonce: {
+    type: Number,
+    required: true,
+    default: () => Math.floor(Math.random() * 1000000),
+  },
+  publicAddress: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
 });
 
 const User = mongoose.model("users", UserSchema);
